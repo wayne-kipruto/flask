7a469dc6a791ef140e46b06a6c81ed3f3a902ec4
wayne-kipruto <wayne.kipruto@strathmore.edu>
2021-05-30 18:35:54 +0300

    adds additional files to the repository

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///root.db'
+app.config ['SQLALCHEMY_DATABASE_URI'] ='sqlite:///root.db'
 
 db=SQLAlchemy(app)
 
@@ -19,21 +19,21 @@ class User(db.Model):
     return '<Contact %r>' & self.id
 
 @app.route("/")
-def index():
+def home():
     return render_template('index.html')
 
 
 @app.route('/contact',methods=['POST','GET'])
 
 def contact():
-  if request.method=='POST':
+  if request.method =='POST':
       email = request.form.get('email')
       firstname = request.form.get('firstname')
       lastname = request.form.get('lastname')
-      message= request.message.form.get('message')
-      new_contact = User(email=email,firstname=firstname,lastname=lastname,message=message)
+      message = request.message.form.get('message')
+      new_contact = User(email=email, firstname=firstname, lastname=lastname, message=message)
   try:
-      db.session.add(User)
+      db.session.add(new_contact)
       db.session.commit()
       return redirect('/')   
   except:
